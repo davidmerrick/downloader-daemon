8e68986399d08bf9359928ae7ce74dd6bc28b4ca
David Merrick <david@david-merrick.com>
2014-08-20 10:10:40 -0700

    Working on daemon

--- downloader.py
+++ downloader.py
@@ -2,33 +2,36 @@
 # Written by David Merrick on 8-20-14
 
 import subprocess
-import Pyro.core
+import Pyro4
 import threading
 import time
 
 class Downloader(Pyro.core.ObjBase):
-        'Daemon for downloading a queue of files'
+        '''Daemon for downloading a queue of files'''
 
         def __init__(self):
                 Pyro.core.ObjBase.__init__(self)
 
 		self.__download_destination_directory = "/media/usbstick/downloads"
                 self.__urls_to_download = []
-	 	self.__current_time = time.strftime("%H")			
+	 	self.__current_time = "None"			
 
 		# Spawn threads
 		
-		self.__dl_thread = threading.Thread(target=self.download_files)
-                self.__time_thread = threading.Thread(target=self.__keep_time)
+		#self.__dl_thread = threading.Thread(target=self.download_files)
+                #self.__time_thread = threading.Thread(target=self.__keep_time)
 
 	def add_url(self, url):
 		self.__urls_to_download.append(url)
 		return "Currently downloading: " + str(self.__urls_to_download)
 
-	#def run(self):
+	def run(self):
 		# Spawn a thread for downloading files and one to monitor the time
-	#	dl_thread = threading.Thread(target=self.download_files)
-	#	time_thread = threading.Thread(target=self.keep_time)	
+		print("Ready")
+	 	self.__current_time = time.strftime("%H")			
+		
+		#dl_thread = threading.Thread(target=self.download_files)
+		#time_thread = threading.Thread(target=self.keep_time)	
 
 	def __keep_time(self):
 		time.sleep(10)
@@ -56,8 +59,9 @@ class Downloader(Pyro.core.ObjBase):
                 self.__urls_to_download = self.__urls_to_download[1:]
 
 Pyro.core.initServer()
-daemon=Pyro.core.Daemon()
-uri=daemon.connect(Downloader(),"downloader")
+downloader = Downloader()
+daemon=Pyro4.Daemon()
+uri=daemon.register(downloader)
 
 print "The daemon runs on port:",daemon.port
 print "The object's uri is:",uri
