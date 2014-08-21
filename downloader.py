ce51ba89a7029faacec764c17ab1ce0f0c0c0a94
David Merrick <david@david-merrick.com>
2014-08-21 06:08:55 -0700

    Refactoring to get rid of keep_time thread

--- downloader.py
+++ downloader.py
@@ -12,9 +12,13 @@ class Downloader(Pyro.core.ObjBase):
         def __init__(self):
                 Pyro.core.ObjBase.__init__(self)
 
+		# Times to do downloading
+		
+		self.__dl_start_hour = 0
+		self.__dl_end_hour = 6
+
 		self.__download_destination_directory = "/media/usbstick/downloads"
                 self.__urls_to_download = []
-	 	self.__current_time = "None"			
 
 		# Instantiate threads
 		
@@ -26,18 +30,28 @@ class Downloader(Pyro.core.ObjBase):
 		return "Currently downloading: " + str(self.__urls_to_download)
 
 	def run(self):
+
 		# Spawn a thread for downloading files and one to monitor the time
+
 		print("Ready")
 		
-		# Spawn threads		
+		# Spawn download thread	
 
 		self.__dl_thread = Thread(target=self.__download_files_thread)
-		self.__time_thread = Thread(target=self.__keep_time_thread)	
-		self.__dl_thread.start()
-		self.__time_thread.start()
+
+		# Go into a loop, checking the time. Start and stop downloads based on this
+		
+		while True:
+			current_hour = time.strftime("%H")
+			if int(current_hour) == self.__dl_start_hour:
+				self.__dl_thread.start()
+			elif int(current_hour) == self.__dl_end_hour:
+				self.__dl_thread.stop()
 
 	def stop(self):
+
 		# Kill the threads
+
 		print("Killing threads and stopping")
 		
 		if self.__dl_thread:
@@ -46,13 +60,6 @@ class Downloader(Pyro.core.ObjBase):
 		if self.__time_thread:
 			self.__time_thread.join()
 
-	def __keep_time_thread(self):
-		while True:
-			self.__keep_time()
-			time.sleep(300)
-
-	def __keep_time(self):
-		self.__current_time = time.strftime("%H")
 
 	def get_time(self):
 		return self.__current_time
@@ -62,21 +69,25 @@ class Downloader(Pyro.core.ObjBase):
 			self.download_files()
 	
         def download_files(self):
+
 		#Downloads all the files in the list
 
                 for url in self.__urls_to_download:
                         self.__download_file(url)
 
         def __download_file(self, url):
+
                 # Spawn a process to download the file
 
                 proc = subprocess.Popen('wget ' + url + ' -P ' + self.__download_destination_directory, shell=True, stdout=subprocess.PIPE)
                 output = proc.communicate()[0]
                 
 		# Wait until subprocess has finished.
+
                 proc.wait()
 
                 # Remove the URL we just downloaded
+
                 self.__urls_to_download.remove(url)
 
 Pyro.core.initServer()
