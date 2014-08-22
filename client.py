a0819d66539b94654ac170d6252dfff2a97d0301
David Merrick <david@david-merrick.com>
2014-08-22 10:31:50 -0700

    Stripped it down to the bare necessities. No more threading. Just downloading.

--- client.py
+++ client.py
@@ -5,6 +5,8 @@ import Pyro.core
 # you have to change the URI below to match your own host/port.
 downloader = Pyro.core.getProxyForURI("PYROLOC://localhost:7766/downloader")
 
-print "Time: " + downloader.get_time()
 
-#downloader.stop()
+url = "http://www.ada.gov/hospcombrprt.pdf"
+print("Adding URL to download: " + url)
+
+downloader.add_url(url)
--- downloader.py
+++ downloader.py
@@ -1,7 +1,7 @@
+#!/usr/bin/python
+
 import subprocess
 import Pyro.core
-import threading
-import time
 
 class Downloader(Pyro.core.ObjBase):
     '''Daemon for downloading a queue of files'''
@@ -9,77 +9,16 @@ class Downloader(Pyro.core.ObjBase):
     def __init__(self):
         Pyro.core.ObjBase.__init__(self)
 
-        # Times to do downloading
-
-        self.__dl_start_hour = 0
-        self.__dl_end_hour = 6
-
-        self.__download_destination_directory = "/home/david/downloads"
+        self.__download_destination_directory = "/Users/david/Projects/downloader-daemon/downloads/"
         self.__urls_to_download = []
-
-        # Thread that does the downloading
-
-        self.__dl_thread = None
 
     def add_url(self, url):
         self.__urls_to_download.append(url)
-        return "Currently downloading: " + str(self.__urls_to_download)
 
     def run(self):
-
-        # Spawn a thread for downloading files and one to monitor the time
-
         print("Ready")
-
-        # Spawn download thread	and stop event (this pauses the thread during times when it shouldn't be downloading)
-
-        dl_stop = threading.Event()
-        #self.__dl_thread = threading.Thread(target=self.__download_files_thread, args={dl_stop})
-        self.__dl_thread = threading.Thread(target=self.__test_thread, args={dl_stop})
-
-        # Go into a loop, checking the time. Start and stop downloads based on this
-
-        self.__dl_thread.start()
-
         while True:
-            #current_hour = time.strftime("%H")
-            #if int(current_hour) == self.__dl_start_hour:
-                # Generate stop event
-            #	dl_stop.set()
-            #elif int(current_hour) == self.__dl_end_hour:
-                # Unset stop event
-            #	dl_stop.clear()
-
-            # Sleep for 5 minutes
-
-            print("Sleeping for 30 seconds")
-            time.sleep(10)
-            print("Setting event flag")
-            dl_stop.set()
-
-    def stop(self):
-
-        # Kill the threads
-
-        print("Killing threads and stopping")
-
-        if self.__dl_thread:
-            self.__dl_thread.join()
-
-    def get_time(self):
-        return self.__current_time
-
-    def __test_thread(self, stop_event):
-        while True:
-            while not stop_event.is_set():
-                time.sleep(20)
-            time.sleep(5)
-
-    def __download_files_thread(self, stop_event):
-        while True:
-            while not stop_event.is_set():
-                self.download_files()
-            time.sleep(5 * 60)
+            self.download_files()
 
     def download_files(self):
 
@@ -106,4 +45,9 @@ class Downloader(Pyro.core.ObjBase):
 
 Pyro.core.initServer()
 downloader = Downloader()
+daemon = Pyro.core.Daemon()
+uri = daemon.connect(downloader, "downloader")
+print("The daemon runs on port: " + daemon.port)
+print("The object's uri is: " + uri)
+
 downloader.run()
