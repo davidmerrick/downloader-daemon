7cc06a9e58c3e243b4c9ed99e8c8651bfac108f9
David Merrick <david@david-merrick.com>
2014-08-22 15:32:58 -0700

    Actually, now it's fully daemonized

--- downloader.py
+++ downloader.py
@@ -4,6 +4,7 @@ import subprocess
 import Pyro.core
 import threading
 from daemon import Daemon
+import sys
 
 class DownloaderDaemon(Daemon):
     def run(self):
@@ -55,5 +56,20 @@ class Downloader(Pyro.core.ObjBase):
 
         self.__urls_to_download.remove(url)
 
-downloaderDaemon = DownloaderDaemon('/home/david/downlaoder.pid')
-downloaderDaemon.start()
+
+if __name__ == "__main__":
+        daemon = DownloaderDaemon('/tmp/downloader-daemon.pid')
+        if len(sys.argv) == 2:
+                if 'start' == sys.argv[1]:
+                        daemon.start()
+                elif 'stop' == sys.argv[1]:
+                        daemon.stop()
+                elif 'restart' == sys.argv[1]:
+                        daemon.restart()
+                else:
+                        print "Unknown command"
+                        sys.exit(2)
+                sys.exit(0)
+        else:
+                print "usage: %s start|stop|restart" % sys.argv[0]
+                sys.exit(2)
