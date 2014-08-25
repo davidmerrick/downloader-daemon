5d92cac9265c97586131b96bc80121edca30f2dc
David Merrick <david@david-merrick.com>
2014-08-25 09:59:58 -0700

    Changed client to be able to accept argv's

--- client.py
+++ client.py
@@ -1,12 +1,31 @@
 #!/usr/bin/python
 
 import Pyro.core
+import sys
 
-# you have to change the URI below to match your own host/port.
-downloader = Pyro.core.getProxyForURI("PYROLOC://localhost:7766/downloader")
+def add_url(downloader, url):
+	print("Adding URL to download: " + url)
+	downloader.add_url(url)
+	
+def start_downloads(downloader):
+	print("Starting downloads")
+	downloader.start_downloads()
 
 
-url = "http://www.ada.gov/hospcombrprt.pdf"
-print("Adding URL to download: " + url)
+if __name__ == "__main__":
 
-downloader.add_url(url)
+	downloader = Pyro.core.getProxyForURI("PYROLOC://localhost:7766/downloader")
+	
+	if not downloader:
+		print("Error: downloader server is not running. Start the server and then try again.")
+		exit(1)
+
+	if len(sys.argv) >= 2:
+		if sys.argv[1] == 'add-url':
+			url = sys.argv[2]
+			add_url(downloader, url)
+		if sys.argv[1] == 'start-downloads':
+			start_downloads(downloader)		
+	else:
+		print("Usage: add-url <URL> | start-downloads")
+
